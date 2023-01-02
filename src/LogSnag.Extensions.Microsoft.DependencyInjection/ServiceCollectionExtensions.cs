ff5cdd2c0a6d2f8e5a12ae1fe2939128733950fd
Joao Correia <joaope@gmail.com>
2023-01-02 05:09:52 +0000

    update extension

--- src/LogSnag.Extensions.Microsoft.DependencyInjection/ServiceCollectionExtensions.cs
+++ src/LogSnag.Extensions.Microsoft.DependencyInjection/ServiceCollectionExtensions.cs
@@ -4,6 +4,13 @@ namespace LogSnag;
 
 public static class ServiceCollectionExtensions
 {
-    public static IHttpClientBuilder AddLogSnagHttpClient(this IServiceCollection services) => 
-        services.AddHttpClient<ILogSnagHttpClient, LogSnagHttpClient>();
+    public static IHttpClientBuilder AddLogSnagHttpClient(this IServiceCollection services, string apiToken)
+    {
+        if (string.IsNullOrWhiteSpace(apiToken))
+        {
+            throw new ArgumentException("Value cannot be null or whitespace.", nameof(apiToken));
+        }
+
+        return services.AddHttpClient<ILogSnagHttpClient, LogSnagHttpClient>(client => new LogSnagHttpClient(client, apiToken));
+    }
 }
