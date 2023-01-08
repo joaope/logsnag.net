024de72fc4609a403c8c42ea229134662328bb07
Joao Correia <joaope@gmail.com>
2023-01-08 03:10:41 +0000

    more tests

--- tests/LogSnag.Tests.Unit/LogSnagResponseExceptionTests.cs
+++ tests/LogSnag.Tests.Unit/LogSnagResponseExceptionTests.cs
@@ -18,7 +18,7 @@ public sealed class LogSnagResponseExceptionTests
     }
 
     [Fact]
-    public void UnparsableError()
+    public void WithBody()
     {
         const string responseContent = @"
 {
@@ -53,6 +53,69 @@ public sealed class LogSnagResponseExceptionTests
                 "Validation Error",
                 new LogSnagErrorResponse.Validations(
                     null,
+                    new[]
+                    {
+                        new LogSnagErrorResponse.Validations.Item(
+                            "event",
+                            "too_small",
+                            "Event name may not be an empty string"),
+                        new LogSnagErrorResponse.Validations.Item(
+                            "tags.tag-1",
+                            "custom",
+                            "Tag keys must be lowercase characters, or dashes")
+                    })),
+            exception.Error);
+    }
+
+    [Fact]
+    public void WithBodyAndHeaders()
+    {
+        const string responseContent = @"
+{
+    ""message"": ""Validation Error"",
+    ""validation"": {
+        ""body"": [
+            {
+                ""path"": ""event"",
+                ""type"": ""too_small"",
+                ""message"": ""Event name may not be an empty string""
+            },
+            {
+                ""path"": ""tags.tag-1"",
+                ""type"": ""custom"",
+                ""message"": ""Tag keys must be lowercase characters, or dashes""
+            }
+        ],
+        ""headers"": [
+            {
+                ""path"": ""authorization"",
+                ""type"": ""invalid_type"",
+                ""message"": ""Authorization header must be a valid Bearer token""
+            }
+        ]
+    }
+}";
+
+        var exception = new LogSnagResponseException(
+            "error message",
+            responseContent,
+            new HttpResponseMessage(HttpStatusCode.BadRequest)
+            {
+                Content = new StringContent(responseContent)
+            });
+
+        Assert.Equal("error message", exception.Message);
+        Assert.Equal(HttpStatusCode.BadRequest, exception.StatusCode);
+        Assert.Equivalent(new LogSnagErrorResponse(
+                "Validation Error",
+                new LogSnagErrorResponse.Validations(
+                    new []
+                    {
+                        new LogSnagErrorResponse.Validations.Item(
+                            "authorization",
+                            "invalid_type",
+                            "Authorization header must be a valid Bearer token")
+                    },
                     new[]
                     {
                         new LogSnagErrorResponse.Validations.Item(
